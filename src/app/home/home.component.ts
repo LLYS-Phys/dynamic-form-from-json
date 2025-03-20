19dfad074401401a4bd2ec469a4f67c8fd3ca8a2
LLYS-Phys <lachyordanov@gmail.com>
2025-03-20 18:46:08 +0200

    add some basics for the functionalities

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormConfig } from '../models/form.model';
+import { helperFunctions } from '../helpers/helper.functions';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  constructor (private helperFunctions: helperFunctions) {}
+
+  invalidJson = false
+
+  jsonForm = new FormGroup({
+    json: new FormControl('', [Validators.required])
+  })
+
+  generatedFormJson: FormConfig | null = null
+
+  valueChanged() {
+    this.invalidJson = false
+  }
+
+  submitJSON(event: Event) {
+    event.preventDefault()
+
+    if (!this.helperFunctions.isJsonString(this.jsonForm.controls.json.value!)) {
+      this.invalidJson = true
+      return
+    }
+
+    this.generatedFormJson = JSON.parse(this.jsonForm.controls.json.value!)
+    
+    console.log(this.generatedFormJson)
+  }
+}
+
+/*
+
+Test JSON:
+
+{
+    "fields": [
+      { "type": "text", "label": "First Name", "name": "firstName" },
+      { "type": "text", "label": "Last Name", "name": "lastName" },
+      { "type": "dropdown", "label": "Role", "name": "role", "options": ["User", "Admin"] }
+    ]
+}
+
+*/
---
+++ src/app/models/form.model.ts
@@ -0,0 +1,15 @@
+export interface FieldConfig {
+    type: 'text' | 'textarea' | 'dropdown' | 'checkbox' | 'radio' | 'custom';
+    label: string;
+    name: string;
+    options?: string[];
+    validations?: { type: string; value?: any; message: string }[];
+}
+
+export interface FormConfig {
+    fields: FieldConfig[];
+    groups?: {
+      title: string;
+      fields: FieldConfig[];
+    }[];
+}  
